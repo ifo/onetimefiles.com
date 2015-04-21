f337ec0458cc9827b3a4dd7f7d6a154990db6717
Steve McCarthy <steve@redlua.com>
2015-04-21 14:28:53 -0400

    Put saved files in subdir rather than base dir

--- src/Main.hs
+++ src/Main.hs
@@ -10,6 +10,7 @@ import Data.Text (unpack)
 import Control.Monad.IO.Class (liftIO)
 import System.Directory (copyFile)
 import Data.Maybe (fromJust)
+import Data.Monoid ((<>))
 
 main :: IO ()
 main =
@@ -24,7 +25,8 @@ main =
       -- TODO save it to a specific subdirectory or other location
       liftIO $ copyFile
         (uf_tempLocation $ fromJust $ H.lookup "file" file)
-        (unpack $ uf_name $ fromJust $ H.lookup "file" file)
+        (unpack "tempfiles/" <>
+          (unpack $ uf_name $ fromJust $ H.lookup "file" file))
       case H.lookup "file" file of
         Nothing -> redirect "/"
         Just uf -> text "saved"
